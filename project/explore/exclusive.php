b869e8108cdb82e3dd71115ec0e4b42359f3d6af
aryanvalvi <94731720+aryanvalvi@users.noreply.github.com>
2023-02-22 20:27:34 +0530

    Add files via upload

---
+++ project/explore/exclusive.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>book</title>
+
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="index.css">
+
+
+</head>
+<body>
+
+   <!-- Header section start -->
+   <section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+     
+   <nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">packages</a>
+    <a href="book.php">book</a>
+   </nav>
+     
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+   </section>
+
+     <!-- header section ends -->
+    
+      
+ <div class="services">
+<section class="pricecol">
+<div class="icon">
+<i class="icon-magic"></i>
+</div>
+<h3><Span>Basic</span></h3>
+<ul>
+    <li>3 Days</li>
+<li>3 Star hotel</li>
+<li>(breakFast,lunch,dinner includes)</li>
+<li>Free wifi</li>
+<li>personal Cab</li>
+<li>Nearby Place with cab  </li>
+<li><a href="">learn more</a></li>
+<li>₹19,999</li>
+
+</ul>
+<p>
+<a class="buttone" href='../book.php'><span>Purchase</span></a>
+</p>
+</section>
+
+<section class="pricecol">
+<div class="icon">
+<i class="icon-leaf"></i>
+</div>
+<h3><Span>Medium</span></h3>
+<ul>
+<li>3 days</li>
+<li>4 star hotel</li>
+<li>Hoti moti palace</li>
+<li>(breakFast,lunch,dinner includes)</li>
+<li>Extra complementary Food Items included </li>
+<li>personal Cab service </li>
+<li>visit Taj With personal Guide</li>
+<li>visit various Nearby Plcaes</li>
+<li><a href="">learn more</a></li>
+<li>₹24,999</li>
+</ul>
+<p>
+<a class="buttone" href='../book.php'><span>Purchase</span></a>
+</p>
+</section>
+
+<section class="pricecol">
+<div class="icon">
+<i class="icon-link"></i>
+</div>
+<h3><Span>Delux</span></h3>
+<ul>
+<li>5 days</li>
+<li>5 star Hotel</li>
+<li>Taj castle a Boutique</li>
+<li>(breakFast,lunch,dinner includes)</li>
+<li>Extra complementary Food Items included </li>
+<li>personal Cab service </li>
+<li>visit Taj With personal Guide</li>
+<li>visit various Nearby Plcaes</li>
+<li>Indian Tradition function </li>
+<li>personal photography + videography</li>
+<li><a href="">learn more</a></li>
+<li>₹29,999</li>
+</ul>
+<p>
+<a class="buttone" href='../book.php'><span>Purchase</span></a>
+</p>
+</section>
+</div>
+
+     
+
+    
+    <!-- footer section start -->
+
+    <section class="footer">
+
+    <div class="box-container">
+
+    <div class="box">
+      <h3>quick links</h3>
+      <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>about</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>packages</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>book</a>
+       
+    </div>
+
+    <div class="box">
+      <h3>extra links</h3>
+      <a href="#"><i class="fas fa-angle-right"></i> ask question</a>
+      <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+      <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+      <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+    </div>
+
+    <div class="box">
+      <h3>contact info</h3>
+      <a href="#"><i class="fas fa-phone"></i> +123-456-789</a>
+      <a href="#"><i class="fas fa-phone "></i> +111-222-333</a>
+      <a href="#"><i class="fas fa-envelope"></i> atharvacoe@gmail.com</a>
+      <a href="#"><i class="fas fa-map"></i> mumbai , india-400101</a>
+    </div>
+
+    <div class="box">
+      <h3>follow us</h3>
+      <a href="#"><i class="fab fa-facebook-f"></i> facebook</a>
+      <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+      <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+      <a href="#"><i class="fab fa-linkedin"></i> linkdin</a>
+    </div>
+
+    </div>
+
+    <div class="credit"> created by <span> Group 31 </span> | all rights reserved</div>
+    </section>
+
+    <!-- footer section ends -->
+   
+
+
+
+ 
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+
+  <!-- custom js file link -->
+  <script src="../js/script.js"></script>
+</body>
+</html>
---
+++ project/explore/taj_mahal.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>book</title>
+
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="index.css">
+
+
+</head>
+<body>
+
+   <!-- Header section start -->
+   <section class="header">
+
+   <a href="home.php" class="logo">travel.</a>
+     
+   <nav class="navbar">
+    <a href="home.php">home</a>
+    <a href="about.php">about</a>
+    <a href="package.php">packages</a>
+    <a href="book.php">book</a>
+   </nav>
+     
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+   </section>
+
+     <!-- header section ends -->
+    
+      
+
+       <img class='abc' src="../images/taj.jpg" alt="">
+      <div class='taj-container'>
+        <div class='cont1'>
+          <h1>Taj Mahal</h1>
+          <p>The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor, Shah Jahan (reigned from 1628 to 1658), to house the tomb of his favourite wife, Mumtaz Mahal. The tomb is the centrepiece of a 17-hectare (42-acre)complex, which includes a mosque and a guest house, and is set in formal gardens bounded on three sides by a crenellated wall.</p>
+
+        </div>
+        <div class='cont2'>
+            <div class='cont41'>
+              <div class='cont21'>
+
+             <img class='png' src="../images/aero.png" alt="">
+             </div>
+             <div class='cont31'>
+            
+             <h1>How to Reach:</h1>
+             <h2>By Air</h2>
+             <p>It takes 12 minutes to travel from Taj Mahal to Kheria Airport. There is a distance of 10 km between the Taj Mahal and the Kheria Airport  </p>
+          </div>
+         </div>
+           
+          <div class='cont41'>
+            <div class='cont21'> <img class='png' src="../images/train.png" alt=""></div>
+             <div class='cont31'>
+                    <h2>by Train</h2>
+                    <p>There is a good network of trains connecting Agra with the rest of the country. Apart from the main railway station of Agra Cantonment, there are other two stations also, that of Raja-ki-Mandi and Agra Fort. The main trains connecting Agra with Delhi are Palace on Wheels, Shatabdi, Rajdhani, and Taj Express.
+                    </p>  
+          </div>
+          </div>
+         <div class='cont41'>
+          <div class='cont21'>
+
+            <img class='png' src="../images/bus.png" alt="">
+          </div>
+          <div class='cont31'>
+
+            <h2>by Road</h2>
+            <p>There are regular bus services from Agra to a number of important cities. The main bus stands of Idgah and ISBT has a number of buses running for Delhi, Jaipur, Mathura, Fatehpur-Sikri, etc. if you are coming from Delhi to Agra. you can hire Delhi to Agra Taxi on specail discounts.
+              </p>
+          </div>
+         </div>
+          
+                
+         </div>
+      </div>
+
+
+      <h1 class='nearbyh1'>Nearby Attractions</h1>
+      <div class='nearby'>
+       <div class="nearbyg1">
+        <h1>Agra Fort</h1>
+        <img src="../images/idk.jpg" alt="">
+        <p>Also known as Lal Qila, Fort Rouge or Red Fort of Agra, the Agra Fort is a UNESCO world heritage site. It is situated at a distance of about 2.5 km northwest of the famous Taj Mahal.</p>
+        <div class='nichewala'>
+          <h1>Fatehpur Sikri</h1>
+          <img src="../images/idk1.jpg" alt="">
+          <p>A city, predominantly made of red sand stone, Fatehpur Sikri was once the capital of Mughal Emperor Akbar. It is now a UNESCO world heritage site and a famous tourist attraction.</p>
+        </div>
+       </div>
+       <div class="nearbyg2">
+        <h1>Akbar's Tomb</h1>
+        <img src="../images/akbar.jpg" alt="">
+        <p>Akbar's tomb is the tomb of the Mughal emperor, Akbar and an important Mughal architectural masterpiece. It was built in 1604–1613 and is situated in 119 acres of grounds.</p>
+        <div class="nichewala">
+          <h1>Itimad-ud-daula's Tomb</h1>
+          <img src="../images/imad.jpg" alt="">
+          <p>This tomb nestled on the bank of the river Yamuna was built by Queen Noor Jahan. It was constructed using yellow, black and white marble inlay, and was hence called the Baby Taj.</p>
+        </div>
+      </div>
+    </div>
+      
+      
+    <div class='button'>
+      <a href='exclusive.php'>
+        <button  class="button-30" role="button">Exclusive Tours</button>
+
+      </a>
+    </div>
+
+    
+    <!-- footer section start -->
+
+    <section class="footer">
+
+    <div class="box-container">
+
+    <div class="box">
+      <h3>quick links</h3>
+      <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>about</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>packages</a>
+    <a href="home.php"><i class="fas fa-angle-right"></i>book</a>
+       
+    </div>
+
+    <div class="box">
+      <h3>extra links</h3>
+      <a href="#"><i class="fas fa-angle-right"></i> ask question</a>
+      <a href="#"><i class="fas fa-angle-right"></i> about us</a>
+      <a href="#"><i class="fas fa-angle-right"></i> privacy policy</a>
+      <a href="#"><i class="fas fa-angle-right"></i> terms of use</a>
+    </div>
+
+    <div class="box">
+      <h3>contact info</h3>
+      <a href="#"><i class="fas fa-phone"></i> +123-456-789</a>
+      <a href="#"><i class="fas fa-phone "></i> +111-222-333</a>
+      <a href="#"><i class="fas fa-envelope"></i> atharvacoe@gmail.com</a>
+      <a href="#"><i class="fas fa-map"></i> mumbai , india-400101</a>
+    </div>
+
+    <div class="box">
+      <h3>follow us</h3>
+      <a href="#"><i class="fab fa-facebook-f"></i> facebook</a>
+      <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+      <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+      <a href="#"><i class="fab fa-linkedin"></i> linkdin</a>
+    </div>
+
+    </div>
+
+    <div class="credit"> created by <span> Group 31 </span> | all rights reserved</div>
+    </section>
+
+    <!-- footer section ends -->
+   
+
+
+
+ 
+
+<!-- swiper js link -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+
+  <!-- custom js file link -->
+  <script src="../js/script.js"></script>
+</body>
+</html>
